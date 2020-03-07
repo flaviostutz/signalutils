a500f59e21b603272375d4d94a1a2a2bb3eb6a13
flaviostutz <flaviostutz@gmail.com>
2020-03-07 12:27:58 -0300

    initial version

--- example/go.mod
+++ example/go.mod
@@ -2,6 +2,6 @@ module example
 
 go 1.14
 
-replace github.com/flaviostutz/signalutils => ../
-
 require github.com/flaviostutz/signalutils v0.0.0-00010101000000-000000000000
+
+replace github.com/flaviostutz/signalutils => ../
--- moving_average.go
+++ moving_average.go
@@ -1,19 +1,81 @@
 package signalutils
 
+import (
+	"math"
+	"time"
+)
+
 //MovingAverage running moving averager
 type MovingAverage struct {
-	Size            int
-	Samples         []float64
-	lastResultValid bool
-	lastResult      float64
+	Size                      int
+	Samples                   []float64
+	lastResultValid           bool
+	lastResult                float64
+	samplesTimeUnixNano       []int64
+	samplesDurationNano       int64
+	lastSampleTimeUnixNano    int64
+	minTimeNanoBetweenSamples int64
 }
 
 //NewMovingAverage creates a new moving averager with a fixed size
 func NewMovingAverage(size int) MovingAverage {
 	return MovingAverage{
-		Samples:         make([]float64, size),
-		lastResultValid: false,
+		Samples:             make([]float64, size),
+		lastResultValid:     false,
+		samplesDurationNano: -1,
 	}
+}
+
+//NewMovingAverageTimeWindow creates a new moving averager that will average samples no older than 'samplesDuration', limiting the number of samples to 'maxSamples' in time window. If two consecutive samples are added to the averager in a period less than duration/maxSamples, it will be ignored.
+func NewMovingAverageTimeWindow(samplesDuration time.Duration, maxSamples int) MovingAverage {
+	minTime := samplesDuration.Nanoseconds() / int64(maxSamples)
+	return MovingAverage{
+		Samples:                   make([]float64, maxSamples),
+		samplesDurationNano:       samplesDuration.Nanoseconds(),
+		samplesTimeUnixNano:       make([]int64, maxSamples),
+		lastResultValid:           false,
+		minTimeNanoBetweenSamples: minTime,
+		lastSampleTimeUnixNano:    0,
+	}
+}
+
+//AddSample adds a new sample to the moving average. If there is more than 'size' samples, the oldest sample will be removed. If this is a timed window averager and the last sample was added in less than sampleDurate/maxSamples time, it will be ignored.
+func (m *MovingAverage) AddSample(value float64) bool {
+	if m.samplesDurationNano != -1 {
+		if (time.Now().UnixNano() - m.lastSampleTimeUnixNano) < m.minTimeNanoBetweenSamples {
+			return false
+		}
+	}
+
+	m.lastSampleTimeUnixNano = time.Now().UnixNano()
+
+	if m.Size < len(m.Samples) {
+		m.Size = m.Size + 1
+	} else {
+		//put new sample in tail
+		for i := 0; i < len(m.Samples)-1; i++ {
+			m.Samples[i] = m.Samples[i+1]
+		}
+	}
+	m.Samples[m.Size-1] = value
+
+	//time window
+	if m.samplesDurationNano != -1 {
+		m.samplesTimeUnixNano[m.Size-1] = time.Now().UnixNano()
+	}
+	m.lastResultValid = false
+	return true
+}
+
+//AddSampleIfNearAverage Add sample only if its value is near current average to avoid espurious samples to be added to the average.
+//avgDiff 1 means samples between [-currentAvg, +currentAvg] will be accepted.
+//Returns true if sample was accepted
+func (m *MovingAverage) AddSampleIfNearAverage(value float64, avgDiff float64) bool {
+	avg := m.Average()
+	if math.Abs(avg-value) <= (avg * avgDiff) {
+		return m.AddSample(value)
+	}
+	return false
 }
 
 //Average computes average with current samples in fixed length list
@@ -24,26 +86,18 @@ func (m *MovingAverage) Average() float64 {
 	if !m.lastResultValid {
 		sum := 0.0
 		for i := 0; i < m.Size; i++ {
+			if m.samplesDurationNano != -1 {
+				//skip this sample if too old
+				if (time.Now().UnixNano() - m.samplesTimeUnixNano[i]) > m.samplesDurationNano {
+					continue
+				}
+			}
 			sum = sum + m.Samples[i]
 		}
 		m.lastResult = sum / float64(m.Size)
 		m.lastResultValid = true
 	}
 	return m.lastResult
-}
-
-//AddSample adds a new sample to the moving average. If there is more than 'size' samples, the oldest sample will be removed
-func (m *MovingAverage) AddSample(value float64) {
-	if m.Size < len(m.Samples) {
-		m.Size = m.Size + 1
-	} else {
-		//put new sample in tail
-		for i := 0; i < len(m.Samples)-1; i++ {
-			m.Samples[i] = m.Samples[i+1]
-		}
-	}
-	m.Samples[m.Size-1] = value
-	m.lastResultValid = false
 }
 
 //Reset internal samples
